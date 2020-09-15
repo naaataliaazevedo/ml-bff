862b2f8ff7c7b32e289c359d69980c2e2cecce80
Natália Azevedo <naaatalia.azevedo@live.com>
2020-09-15 00:10:29 -0300

    feat(search): creating search schema and resolver

--- src/resolvers/Search.js
+++ src/resolvers/Search.js
@@ -1,9 +1,16 @@
+const books = [{id: 2}, {id: 3}]
+
+
 module.exports = {
   Query: {
     search: async (_, __, { dataSources }) => {
-      const response = await dataSources.mlAPI.getProductSearch();
-      console.log(response);
-      return response;
-    }
+      const productList = await dataSources.mlAPI.getProductSearch();
+      // console.log('category list', productList.available_filters);
+      // console.log(productList.results[2]);
+
+      const getResults = productList.results;
+
+      return getResults;
+    },
   },
 };
