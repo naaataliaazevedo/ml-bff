3fb0e514bc98ca34883a27a9587b4f17340b02dc
Natália Azevedo <naaatalia.azevedo@live.com>
2020-09-14 21:27:38 -0300

    feat(node): removing node modules

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const productionEnvironment = {
   // NODE_ENV: process.env.NODE_ENV || "development",
   // PORT: process.env.PORT || 4000,
-  SORTING_HAT_URL:
-    process.env.SORTING_HAT_URL || "https://sorting-hat.us.stg.gympass.cloud",
+  //SORTING_HAT_URL: "https://sorting-hat.us.stg.gympass.cloud",
+  ML_API_URL: 'https://api.mercadolibre.com'
 };
 
 module.exports = productionEnvironment;
