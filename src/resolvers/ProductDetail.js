56575d4cbd8bd042e1b975529a7e03ec6c58c005
Natália Azevedo <naaatalia.azevedo@live.com>
2020-09-18 10:17:43 -0300

    feat(detail): create resolver and schema to product detail

--- src/resolvers/ProductDetail.js
+++ src/resolvers/ProductDetail.js
@@ -2,7 +2,6 @@ module.exports = {
   Query: {
     product: async (_, { input }, { dataSources }) => {
       const product = await dataSources.mlAPI.getProductDetails(input);
-      console.log('product', product, input);
 
       return product;
     },
