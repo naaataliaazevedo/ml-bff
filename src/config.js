782befa3bfdfee3e4d7c24cdf15114592675b150
Natália de Azevedo <naaatalia.azevedo@live.com>
2024-01-04 11:48:16 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const productionEnvironment = {
   // NODE_ENV: process.env.NODE_ENV || "development",
   // PORT: process.env.PORT || 4000,
-  //SORTING_HAT_URL: "https://sorting-hat.us.stg.gympass.cloud",
+  //SORTING_HAT_URL: "https://sorting-hat.us.stg.example.cloud",
   ML_API_URL: 'https://api.mercadolibre.com'
 };
 
